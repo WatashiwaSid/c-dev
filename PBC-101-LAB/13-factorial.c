dbc62c5a090cf6b22fa5137d0023fc435e8c7cd3
Siddhant N <71310931+WatashiwaSid@users.noreply.github.com>
2022-11-30 21:05:52 +0530

    Add files via upload

---
+++ PBC-101-LAB/13-factorial.c
@@ -0,0 +1,16 @@
+// Objective : To find the factorial of a given number.
+// Author : Siddhant Nautiyal
+
+#include<stdio.h>
+void main()
+{
+    int num,count;
+    long long int factorial=1; //Please do not use long integers unless required.
+    printf("Enter any number : ");
+    scanf("%d", &num);
+    for(count=1;count<=num;count++)
+    {
+        factorial=factorial*count;
+    }
+    printf("Factorial of %d is %llu", num, factorial);
+}
